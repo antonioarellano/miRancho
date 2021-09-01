3fa34c8d428bae474ba67e88a56aab46b3c9a222
antonioarellano <doncrow@GA-78LMT-USB3-464d4867.localdomain>
2021-09-01 10:51:15 -0500

    App de Java para escritorio

---
+++ MiRancho-Java/src/Main.java
@@ -0,0 +1,11 @@
+
+public  class Main {
+    
+    public static void main(String args[])
+    {
+       jfPrincipal ventana = new jfPrincipal();
+       
+       ventana.setVisible(true);
+    }
+
+}
