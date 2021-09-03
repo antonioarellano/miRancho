db6a0d84cd7acb84d29ff41d5e7c49baf5cdecba
antonioarellano <doncrow@GA-78LMT-USB3-464d4867.localdomain>
2021-09-03 11:16:33 -0500

    Native Base

--- miRancho-React/App.js
+++ miRancho-React/App.js
@@ -1,13 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Button, NativeBaseProvider, Box, Center } from 'native-base';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider >
+      <Center>
+        <Box><Text>Hola</Text></Box>
+        <Button>Hola</Button>
+      </Center>
+    </NativeBaseProvider>
   );
 }
 
